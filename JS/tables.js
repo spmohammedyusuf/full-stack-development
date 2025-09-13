c61b494e8112d2c40eb447bba97dad21a365a05c
spmohammedyusuf <spmohammedyusuf@gmail.com>
2025-09-13 20:37:33 +0530

    tables

---
+++ JS/tables.js
@@ -0,0 +1,8 @@
+for (let i = 1; i <= 10; i++) {
+  console.log("printing multiplication table of  " + i);
+  for (let n = 1; n <= 10; n++) {
+    let result = i * n;
+    console.log(i + " x " + n + " = " + result);
+  }
+  console.log();
+}
